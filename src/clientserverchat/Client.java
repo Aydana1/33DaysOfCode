027466e8fc7aef964bedee3bb1fc3ba192a2fafd
Айдана <aidana.nurakhmetova@nu.edu.kz>
2018-03-20 23:22:49 +0600

    Created a Client objetc with the username

--- src/clientserverchat/Client.java
+++ src/clientserverchat/Client.java
@@ -10,21 +10,99 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author ajdana
  */
-public class Client {
+public class Client implements Runnable {
     
+    private static Socket clientSocket = null;
+    private static BufferedReader inFromUser = null;
+    private static DataOutputStream outToServer = null;
+    private static BufferedReader inFromServer = null;
+    private static String send, rec;
+    
+    private final int portNumber;
+    private final String hostname;
+    private String username;
+    
+    public String getUsername() {
+        return username;
+    }    
+    
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    public Client(String hostname, int portNumber, String username) {
+        this.hostname = hostname;
+        this.portNumber = portNumber;
+        this.username = username;
+    }
+    
+
     public static void main(String argv[]) throws IOException {
         
-        Socket clientSocket = new Socket("localhost", 6789);
+        int portNumber = 6789;
+        String hostname = "localhost";
+        String username = "Anonymous";
+	Scanner scan = new Scanner(System.in);
+		
+	System.out.println("Enter the username: ");
+	username = scan.nextLine();
+  
+        Client client = new Client(hostname, portNumber, username);
         
-        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in)); //read from keyboard
-        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());  // send to server
-        BufferedReader inFromServer =  new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        clientSocket = new Socket("localhost", 6789); 
         
+        inFromUser = new BufferedReader(new InputStreamReader(System.in)); //read from keyboard
+        outToServer = new DataOutputStream(clientSocket.getOutputStream());  // send to server
+        inFromServer =  new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        
+        if(clientSocket != null && inFromServer != null && outToServer != null) {
+            
+            new Thread(client).start();
+              
+            while(true){
+              send = inFromUser.readLine();  //from user
+              outToServer.writeBytes(send + '\n');  //to server
+              break;
+            }
+            
+            clientSocket.close();
+            inFromUser.close();
+            outToServer.close();
+        }
+        
+        scan.close();
+         
+    }
+
+    @Override
+    public void run() {
+             
+        while(true) {
+            try {
+                rec = inFromServer.readLine();  //from server
+            } catch (IOException ex) {
+                Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+            }
+             System.out.println("FROM SERVER: " + rec);
+              
+              if(rec.equals("END")){
+                try {
+                    clientSocket.close();
+                } catch (IOException ex) {
+                    Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                  break;
+               } 
+        }
+    
     }
     
 }
